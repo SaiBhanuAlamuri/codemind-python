ed335d8fc6d8cce0ced0cc1993d33779c9c2a93e
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-14 16:24:02 +0530

    def fun(a):     c=0     r=0     while(a!=0):         r=a%10         c=c+1         a=a//10     return c n=int(input()) ls=list(map(int,input().split())) s=0 for i in range(len(ls)):     if(fun(ls[i])%2==0):         s=s+1 print(s)

--- Find_Numbers_with_Even_Number_of_Digits.py
+++ Find_Numbers_with_Even_Number_of_Digits.py
@@ -1,16 +1,15 @@
 def fun(a):
     c=0
     r=0
-    while a!=0:
+    while(a!=0):
         r=a%10
         c=c+1
         a=a//10
     return c
 n=int(input())
-arr=list(map(int,input().split()))
+ls=list(map(int,input().split()))
 s=0
-for i in range(n):
-    if(fun(arr[i])%2==0):
+for i in range(len(ls)):
+    if(fun(ls[i])%2==0):
         s=s+1
 print(s)
-        
