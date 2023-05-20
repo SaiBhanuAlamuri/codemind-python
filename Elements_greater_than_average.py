93dd59023bec6dcb968d645a7cf8e8bd686952aa
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-20 15:21:39 +0530

    import math n=int(input()) ls=list(map(int,input().split())) c=0 s=0 for i in range(len(ls)):     s=s+ls[i] avg=int(s/n) for i in range(len(ls)):     if(ls[i]>=avg):         c=c+1 print(c)

---
+++ Elements_greater_than_average.py
@@ -0,0 +1,12 @@
+import math
+n=int(input())
+ls=list(map(int,input().split()))
+c=0
+s=0
+for i in range(len(ls)):
+    s=s+ls[i]
+avg=int(s/n)
+for i in range(len(ls)):
+    if(ls[i]>=avg):
+        c=c+1
+print(c)
