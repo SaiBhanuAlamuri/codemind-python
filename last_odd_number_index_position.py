c3e11eda8ded756df6776349682d1d08953be5b5
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-20 12:52:18 +0530

    def fun(n):     flag=0     if(n%2!=0):         flag=1     return flag n=int(input()) ls=list(map(int,input().split())) f=0 for i in range(n-1,0,-1):     if(fun(ls[i])==1):         f=i         break print(i)

---
+++ last_odd_number_index_position.py
@@ -0,0 +1,14 @@
+def fun(n):
+    flag=0
+    if(n%2!=0):
+        flag=1
+    return flag
+n=int(input())
+ls=list(map(int,input().split()))
+f=0
+for i in range(n-1,0,-1):
+    if(fun(ls[i])==1):
+        f=i
+        break
+print(i)
+        
