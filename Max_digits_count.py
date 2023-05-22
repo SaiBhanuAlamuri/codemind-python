a2cfa6039a285e05eb4dfe37224d7896f5a4467f
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-22 15:24:23 +0530

    def count(n):     d=0     while(n!=0):         r=n%10         d=d+1         n=n//10     return d n=int(input()) ls=list(map(int,input().split())) ls1=[] c=0 for i in ls:     ls1.append(count(i)) x=max(ls1) for i in ls:     if(count(i)==x):         c=c+1 print(c)

---
+++ Max_digits_count.py
@@ -0,0 +1,22 @@
+def count(n):
+    d=0
+    while(n!=0):
+        r=n%10
+        d=d+1
+        n=n//10
+    return d
+n=int(input())
+ls=list(map(int,input().split()))
+ls1=[]
+c=0
+for i in ls:
+    ls1.append(count(i))
+x=max(ls1)
+for i in ls:
+    if(count(i)==x):
+        c=c+1
+print(c)
+    
+    
+
+        
