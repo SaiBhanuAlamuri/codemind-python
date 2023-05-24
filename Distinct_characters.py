591e7a3af4942a570053f485014a9f6e1ddd2742
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-24 12:31:19 +0530

    a=input() ar=[] for i in a:     if i.isspace():         continue     if i.lower() not in ar:         ar.append(i.lower()) for i in range(len(ar)):     for j in range(len(ar)-1):         if(ar[j]>ar[j+1]):             ar[j],ar[j+1]=ar[j+1],ar[j] for i in ar:     print(i,end="")

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,16 +1,13 @@
 a=input()
 ar=[]
-for i in range(len(a)):
-    if a[i].isspace():
+for i in a:
+    if i.isspace():
         continue
-    if a[i].upper() not in a[:i] and a[i].upper() not in a[i+1:]:
-        if a[i].lower() not in a[:i] and a[i].lower() not in a[i+1:]:
-            ar.append(a[i])
+    if i.lower() not in ar:
+        ar.append(i.lower())
 for i in range(len(ar)):
     for j in range(len(ar)-1):
-        if ar[j+1]<ar[j]:
+        if(ar[j]>ar[j+1]):
             ar[j],ar[j+1]=ar[j+1],ar[j]
 for i in ar:
     print(i,end="")
-if(len(ar)==0):
-    print(0)
