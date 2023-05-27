661acd7c2cda68b82ff3b6d6af3b4d41560dfa26
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-27 14:37:56 +0530

    def rev(n):     r=0     rev=0     while(n!=0):         r=n%10         rev=rev*10+r         n=n//10     return rev n=int(input()) print(rev(n))

--- reverse_of_a_numbers.py
+++ reverse_of_a_numbers.py
@@ -1,9 +1,10 @@
+def rev(n):
+    r=0
+    rev=0
+    while(n!=0):
+        r=n%10
+        rev=rev*10+r
+        n=n//10
+    return rev
 n=int(input())
-r=0
-rev=0
-while(n!=0):
-    r=n%10
-    rev=rev*10+r
-    n=n//10
-print(rev)
-    
+print(rev(n))
