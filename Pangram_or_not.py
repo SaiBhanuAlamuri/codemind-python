ac1ef0314fa4f35e5474c52d693011eecc2ea910
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-27 10:23:07 +0530

    s=input() arr="abcdefghijklmnopqrstuvwxyz" c=0 v=0 for i in arr:     if i in s or i.upper() in s:         c=c+1     else:         v=1         break if v!=1:     print("True") else:     print("False")

---
+++ Pangram_or_not.py
@@ -0,0 +1,14 @@
+s=input()
+arr="abcdefghijklmnopqrstuvwxyz"
+c=0
+v=0
+for i in arr:
+    if i in s or i.upper() in s:
+        c=c+1
+    else:
+        v=1
+        break
+if v!=1:
+    print("True")
+else:
+    print("False")
