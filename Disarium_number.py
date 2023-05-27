50f7887a6f70147286ac952efe6e82784eaa0049
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-27 13:16:33 +0530

    import math def count(n):     r=0     d=0     while(n!=0):         r=n%10         d=d+1         n=n//10     return d def fun(n):     flag=0     dc=count(n)     r=0     x=n     s=0     while(n!=0):         r=n%10         s=s+pow(r,dc)         dc=dc-1         n=n//10     if x==s:         flag=1     return flag
    n=int(input())
    if(fun(n)==1):
        print("True")
    else:
        print("False")

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,14 +1,31 @@
 import math
-n=int(input())
-st=str(n)
-ls=[]
-s=0
-for i in st:
-    ls.append(int(i))
+def count(n):
+    r=0
+    d=0
+    while(n!=0):
+        r=n%10
+        d=d+1
+        n=n//10
+    return d
+def fun(n):
+    flag=0
+    dc=count(n)
+    r=0
+    x=n
+    s=0
+    while(n!=0):
+        r=n%10
+        s=s+pow(r,dc)
+        dc=dc-1
+        n=n//10
+    if x==s:
+        flag=1
+    return flag
 
-for i in range(len(st)):
-    s=s+pow(ls[i],(i+1))
-if(s==n):
+   
+    
+n=int(input())
+if(fun(n)==1):
     print("True")
 else:
     print("False")
