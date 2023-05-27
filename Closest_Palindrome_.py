11039db3c98b73b39f4a6067d939a02a77bb6e8c
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-27 12:52:47 +0530

    def pal(n):     f=0     r=0     rev=0     x=n     while(n!=0):         r=n%10         rev=rev*10+r         n=n//10     if x==rev:         f=1     return f
    n=int(input())
    fs=0
    ls=0
    for i in range(n-1,-1,-1):
        if (pal(i)==1):
            fs=i
            break
    for i in range(n+1,10000):
        if (pal(i)==1):
            ls=i
            break
    df=n-fs
    dl=ls-n
    if(df>dl):
        print(ls)
    elif(df==dl):
        print("%d %d"%(fs,ls))
    else:
        print(fs)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,30 +1,32 @@
-def fun(n):
+def pal(n):
+    f=0
     r=0
     rev=0
+    x=n
     while(n!=0):
         r=n%10
         rev=rev*10+r
         n=n//10
-    return rev
+    if x==rev:
+        f=1
+    return f
+        
 n=int(input())
-f=0
-l=0
-dl=0
-df=0
-for i in range(n-1,1,-1):
-    if(fun(i)==i):
-        f=i
+fs=0
+ls=0
+for i in range(n-1,-1,-1):
+    if (pal(i)==1):
+        fs=i
         break
-for i in range(n+1,100000):
-    if(fun(i)==i):
-        l=i
+for i in range(n+1,10000):
+    if (pal(i)==1):
+        ls=i
         break
-df=n-f
-dl=l-n
-if(df==dl):
-    print("%d %d"%(f,l))
-elif(df>dl):
-    print("%d"%(l))
+df=n-fs
+dl=ls-n
+if(df>dl):
+    print(ls)
+elif(df==dl):
+    print("%d %d"%(fs,ls))
 else:
-    print("%d"%(f))
-    
+    print(fs)
