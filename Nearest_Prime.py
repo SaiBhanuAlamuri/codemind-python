15ca27e4025df17630f56e0deb2812b2bc1143b5
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-27 23:50:25 +0530

    def fun(n):     c=0     f=0     for i in range(1,n+1):         if(n%i==0):             c=c+1     if c==2:         f=1     return f t=int(input()) f=l=dl=df=0 for l in range(t):     n=int(input())     for i in range(n-1,-1,-1):         if fun(i)==1:             f=i             break     for i in range(n,10000):         if fun(i)==1:             l=i             break     df=n-f     dl=l-n     if(df>dl):         print(l)     elif(dl>df):         print(f)     else:         print(f)

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,40 +1,30 @@
 def fun(n):
     c=0
-    flag=0
+    f=0
     for i in range(1,n+1):
-        if n%i==0:
+        if(n%i==0):
             c=c+1
     if c==2:
-        flag=1
-    else:
-        flag=0
-    return flag
-
+        f=1
+    return f
 t=int(input())
-for i in range(t):
-    f=0
-    l=0
-    dif1=0
-    dif2=0
+f=l=dl=df=0
+for l in range(t):
     n=int(input())
-    for i in range(n-1,1,-1):
-        if(fun(i)==1):
+    for i in range(n-1,-1,-1):
+        if fun(i)==1:
             f=i
             break
-        
     for i in range(n,10000):
-        if(fun(i)==1):
+        if fun(i)==1:
             l=i
             break
-    dl=l-n
     df=n-f
-    if(dl>df):
-        print(f)
-    elif(df>dl):
+    dl=l-n
+    if(df>dl):
         print(l)
+    elif(dl>df):
+        print(f)
     else:
-        if(f>l):
-            print(l)
-        else:
-            print(f)
+        print(f)
         
