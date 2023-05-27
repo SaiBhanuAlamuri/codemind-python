659732fa8867901eabc38ab4aed077afb4ba6cd8
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-27 14:42:23 +0530

    import math n=int(input()) sq=int(pow(n,0.5)) c=0 for i in range(1,sq+1):     if(i*i==n):         c=1 if(c==0):     print("False") else:     print("True")

--- perfect_square_root_or_not.py
+++ perfect_square_root_or_not.py
@@ -1,16 +1,12 @@
-import math
-def fun(n):
-    res=0
-    x=int(pow(n,0.5))
-    for i in range(1,x+1):
-        if(i*i==n):
-            res=1
-    if(res==1):
-        return 1
-    else:
-        return 0
+import math 
 n=int(input())
-if(fun(n)==1):
-    print("True")
-else:
+sq=int(pow(n,0.5))
+c=0
+for i in range(1,sq+1):
+    if(i*i==n):
+        c=1
+if(c==0):
     print("False")
+else:
+    print("True")
+        
