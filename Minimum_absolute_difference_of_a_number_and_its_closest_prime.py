7133720bca54c5719f6154c68386335ce28dcaca
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-27 23:40:38 +0530

    def fun(n):     c=0     f=0     for i in range(1,n+1):         if(n%i==0):             c=c+1     if c==2:         f=1     return f n=int(input()) f=l=dl=df=0 for i in range(n-1,-1,-1):     if fun(i)==1:         f=i         break for i in range(n,10000):     if fun(i)==1:         l=i         break df=n-f dl=l-n print(min(df,dl))

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -1,28 +1,23 @@
 def fun(n):
     c=0
-    flag=0
+    f=0
     for i in range(1,n+1):
         if(n%i==0):
             c=c+1
-    if(c==2):
-        flag=1
-    else:
-        flag=0
-    return flag
-
+    if c==2:
+        f=1
+    return f
 n=int(input())
-f=0
-l=0
-df=0
-dl=0
-for i in range(n-1,1,-1):
-    if(fun(i)==1):
-        f=i;
+f=l=dl=df=0
+for i in range(n-1,-1,-1):
+    if fun(i)==1:
+        f=i
         break
-for i in range(n,100000):
-    if(fun(i)==1):
-        l=i;
+for i in range(n,10000):
+    if fun(i)==1:
+        l=i
         break
 df=n-f
 dl=l-n
 print(min(df,dl))
+    
