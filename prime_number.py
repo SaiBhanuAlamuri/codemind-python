5a65e0891c02b0db274f453e43fb92c4fb5541ec
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-27 14:44:35 +0530

    def fun(n):     c=0     f=0     for i in range(1,n+1):         if(n%i==0):             c=c+1     if c==2:         f=1     return f n=int(input()) if(fun(n)==1):     print("prime") else:     print("not a prime")

--- prime_number.py
+++ prime_number.py
@@ -1,9 +1,15 @@
+def fun(n):
+    c=0
+    f=0
+    for i in range(1,n+1):
+        if(n%i==0):
+            c=c+1
+    if c==2:
+        f=1
+    return f
 n=int(input())
-c=0
-for i in range(1,n+1):
-    if(n%i==0):
-        c=c+1
-if(c==2):
+if(fun(n)==1):
     print("prime")
 else:
     print("not a prime")
+        
