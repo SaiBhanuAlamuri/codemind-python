becfe076586758e932ffbcc75a2d92eecdc349ba
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-31 15:38:26 +0530

    def fun(n):     c=0     f=0     for i in range(1,n+1):         if n%i==0:             c=c+1     if c==2:         f=1     return f n=int(input()) m=int(input()) for i in range(n,m+1):     if fun(i)==1:         print(i)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,16 +1,14 @@
 def fun(n):
     c=0
-    flag=0
+    f=0
     for i in range(1,n+1):
-        if(n%i==0):
+        if n%i==0:
             c=c+1
-    if(c==2):
-        flag=1
-    else:
-        flag=0
-    return flag
+    if c==2:
+        f=1
+    return f
 n=int(input())
 m=int(input())
 for i in range(n,m+1):
-    if(fun(i)==1):
+    if fun(i)==1:
         print(i)
