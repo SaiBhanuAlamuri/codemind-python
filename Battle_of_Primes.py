0af201350a011073398ded915b250f749512970c
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-31 16:09:51 +0530

    def fun(n):     c=f=0     for i in range(1,n+1):         if(n%i==0):             c=c+1     if c==2:         f=1     return f n=int(input()) m=int(input()) f=0 for i in range(1,10000):     if(fun(n+m+i)==1):         f=i         break print(f)

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,25 +1,17 @@
 def fun(n):
-    c=0
-    flag=0
+    c=f=0
     for i in range(1,n+1):
         if(n%i==0):
             c=c+1
-    if(c==2):
-        flag=1
-    else:
-        flag=0
-    return flag
+    if c==2:
+        f=1
+    return f
 n=int(input())
 m=int(input())
-res=0
+f=0
 for i in range(1,10000):
     if(fun(n+m+i)==1):
-        res=i
+        f=i
         break
-print(res)
-    
-    
-        
-        
-    
+print(f)
         
