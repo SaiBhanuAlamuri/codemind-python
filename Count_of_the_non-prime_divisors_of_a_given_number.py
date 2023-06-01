b7f277445e68800cff22adc52a285c74f3b3351b
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-06-01 14:05:54 +0530

    def fun(n):     c=0     f=0     for i in range(1,n+1):         if(n%i==0):             c=c+1     if c==2:         f=1     return f n=int(input()) c=0 for i in range(1,n+1):     if(fun(i)==0 and n%i==0):         c=c+1 print(c)

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,16 +1,15 @@
 def fun(n):
-    flag=0
     c=0
+    f=0
     for i in range(1,n+1):
         if(n%i==0):
             c=c+1
-    if(c==2):
-        flag=1
-    return flag
+    if c==2:
+        f=1
+    return f
 n=int(input())
-ls=[]
+c=0
 for i in range(1,n+1):
-    if n%i==0:
-        if fun(i)==0:
-            ls.append(i)
-print(len(ls))
+    if(fun(i)==0 and n%i==0):
+        c=c+1
+print(c)
